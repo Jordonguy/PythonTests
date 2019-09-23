3851c39cb8a73381d5d7e162a427b16a7cdab03c
Your Name <you@example.com>
2019-09-23 16:46:08 +0100

    Start of 40kDiceRollResolver Project

---
+++ Warhammer40k8th/WoundRoll.py
@@ -0,0 +1,50 @@
+# A simple app that takes users input, rolls dice and informs the user the outcome. (Strength VS Toughness)
+# If S if double or more than T - 2+
+# If S>T - 3+
+# If S=T - 4+
+# If S<T - 5+
+# If S half or less than T - 6+
+# Every roll of 1 automatically fails.
+# Wound rolls (StrengthVsToughness)
+
+import random
+
+dice_rolled = 0
+successful_wounds = 0
+failed_wounds = 0
+successful_hits = int(
+    input("Enter the number of successful hit rolls made : "))
+wep_strength = int(input("Enter the attacking weapons Strength : "))
+#ap_modifer = int(input("Enter any AP modifiers : "))
+target_toughness = int(input("Enter the targets toughness : "))
+print(str(successful_hits) + " hits have been made against a target with " + str(target_toughness) +
+      " toughness with a weapon that has a strength of " + str(wep_strength))
+print("Now rolling " + str(successful_hits) + " dice!.....")
+
+while(successful_hits > 0):
+    random_num = random.randint(1, 6)
+    print(str(random_num) + " was rolled")
+    if(random_num == 1):
+        print("The hit failed to wound the target")
+        failed_wounds += 1
+    elif((wep_strength * 2 >= target_toughness) and (random_num >= 2)):
+        print("The hit successfully wounded the target")
+        successful_wounds += 1
+    elif((wep_strength > target_toughness) and (random_num >= 3)):
+        print("The hit successfully wounded the target")
+        successful_wounds += 1
+    elif((wep_strength == target_toughness) and (random_num >= 4)):
+        print("The hit successfully wounded the target")
+        successful_wounds += 1
+    elif((wep_strength < target_toughness) and (random_num >= 5)):
+        print("The hit successfully wounded the target")
+        successful_wounds += 1
+    elif((wep_strength <= (target_toughness / 2)) and (random_num >= 6)):
+        print("The hit successfully wounded the target")
+        successful_wounds += 1
+    else:
+        print("The hit failed to wound the target")
+        failed_wounds += 1
+    successful_hits -= 1
+print(str(successful_wounds) + " shots successfully wounded the target")
+print(str(failed_wounds) + " failed to wound the target")
