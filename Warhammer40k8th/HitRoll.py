a4709c4166cbb0168c1498e800bd9ddfa487a08a
Your Name <you@example.com>
2019-09-25 17:32:20 +0100

    Notes and Folder clean up

---
+++ Warhammer40k8th/HitRoll.py
@@ -0,0 +1,3 @@
+# This function will take user input on how many shots they are firing at an enemy unit, and the attacking units Balistic Skill
+# The function will roll a dice for each shot, and if the result of the roll is greater or equal to the attacking units balistic skill
+# that shot will be successful, if the result is less than the balistic skill the shot will fail.
---
+++ Warhammer40k8th/SaveRoll.py
@@ -0,0 +1,2 @@
+# Save rolls are for shots that have hit and successfully wounded the intended target, the enemy unit has a Save characteristic.
+# For each successful wound a Save roll is made, if the rolls result is equal or greater than the units Save characteristic that model doesn't suffer a wound, but if the roll is less than the Save that unit suffers a wound.
